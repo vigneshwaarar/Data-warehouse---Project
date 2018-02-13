aa2b95dbbf9d4c52481a9b87cb2cfb34323f150f
vigneshwaarar <Vchidambaramrajakann@hawk.iit.edu>
2018-02-12 19:17:29 -0600

    adding datawarehouse project

---
+++ Pentaho - ETL/documentations/audit-query.sql
@@ -0,0 +1,54 @@
+SELECT group_seq
+     , MAX(IF(table_type = 'source', table_name,  NULL)) AS source
+     , MAX(IF(table_type = 'target', table_name,  NULL)) AS target
+     , SUM(IF(table_type = 'source', audit_value, NULL)) AS source_value
+     , SUM(IF(table_type = 'target', audit_value, NULL)) AS target_value
+     , IF (SUM(IF(table_type = 'source', audit_value, NULL)) = SUM(IF(table_type = 'target', audit_value, NULL)), 'PASS', 'FAIL') audit_result
+  FROM
+       (
+        /* Fact Granuality */
+        SELECT 1 AS group_seq, 'source' AS table_type, 'gs_orders count' AS table_name, COUNT(*) AS audit_value
+          FROM source_db.gs_orders
+         UNION ALL
+        SELECT 1 AS group_seq, 'target' AS table_type, 'fact_orders count' AS table_name, COUNT(*) AS audit_value
+          FROM datamart_db.fact_orders
+
+         UNION ALL
+
+         /* Order Date and Ship Date */
+        SELECT 2 AS group_seq, 'source' AS table_type, 'gs_orders data_diff' AS table_name,
+               SUM(CONVERT(DATE_FORMAT(`Ship Date`, '%Y%m%d'), SIGNED INTEGER) - CONVERT(DATE_FORMAT(`Order Date`, '%Y%m%d'), SIGNED INTEGER)) AS audit_value
+          FROM source_db.gs_orders
+         UNION ALL
+        SELECT 2 AS group_seq, 'target' AS table_type, 'fact_orders date_diff' AS table_name, SUM(ship_date_skey - order_date_skey) AS audit_value
+          FROM datamart_db.fact_orders
+
+         UNION ALL
+
+          /* State Information */
+        SELECT 3 AS group_seq, 'source' AS table_type, 'stg_state count' AS table_name, COUNT(*) AS audit_value
+          FROM stage_db.stg_state
+         UNION ALL
+        SELECT 3 AS group_seq, 'target' AS table_type, 'dim_geography count' AS table_name, COUNT(*) AS audit_value
+          FROM datamart_db.dim_geography
+
+         UNION ALL
+
+          /* Customer Information */
+        SELECT 4 AS group_seq, 'source' AS table_type, 'stg_customer count' AS table_name, COUNT(*) AS audit_value
+          FROM stage_db.stg_customer
+         UNION ALL
+        SELECT 4 AS group_seq, 'target' AS table_type, 'dim_customer count' AS table_name, COUNT(*) AS audit_value
+          FROM datamart_db.dim_customer
+
+         UNION ALL
+
+          /* Address Information */
+        SELECT 5 AS group_seq, 'source' AS table_type, 'stg_address count' AS table_name, COUNT(*) AS audit_value
+          FROM stage_db.stg_address
+         UNION ALL
+        SELECT 5 AS group_seq, 'target' AS table_type, 'dim_address count' AS table_name, COUNT(*) AS audit_value
+          FROM datamart_db.dim_address
+       ) a
+ GROUP BY group_seq
+
